b5daae47245b0153503e73d2bfc32d92c8d56787
agn3050 <agn3050@AG003880>
2018-03-28 14:32:52 -0400

    Add project files.

---
+++ Arrays/ArrayApp.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays {
+  class ArrayApp {
+    static void Main(string[] args) {
+      HighArray highArray = new HighArray(100);
+
+      highArray.insert(11);
+      highArray.insert(22);
+      highArray.insert(33);
+      highArray.insert(44);
+      highArray.insert(55);
+      highArray.insert(66);
+      highArray.insert(77);
+      highArray.insert(88);
+      highArray.insert(99);
+      highArray.insert(100);
+
+      highArray.display();
+
+      int searchKey = 35;
+      if (highArray.find(searchKey)) {
+        Console.WriteLine("Found " + searchKey + " in array");
+      }
+      else {
+        Console.WriteLine("Can't find " + searchKey);
+      }
+
+
+      //delete three items
+      highArray.delete(44);
+      highArray.delete(99);
+      highArray.delete(22);
+
+      highArray.display();
+      Console.ReadKey();
+    }
+  }
+}
---
+++ Arrays/HighArray.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//High-level interface OOP
+namespace Arrays {
+  class HighArray {
+    private long[] _arr; //ref to array
+    private int nElems; //number of data items
+
+    public HighArray(int size) {
+      _arr = new long[size];
+      nElems = 0;
+    }
+    //*********************************************
+    public bool find(long searchKey) {
+      int j;
+      for (j = 0; j < nElems; j++) {
+        if (_arr[j] == searchKey) {
+          break;
+        }
+      }
+      if (j == nElems) {
+        return false;
+      }
+      else {
+        return true;
+      }
+    }
+    //*********************************************
+    public void insert(long value) {
+      _arr[nElems] = value;
+      nElems++;
+    }
+    //*********************************************
+    public bool delete(long value) {
+      int j;
+      for (j = 0; j < nElems; j++) {
+        if (value == _arr[j]) {
+          break;
+        }
+      }
+      if (j == nElems) { //cant find value
+        return false;
+      }
+      else {
+        for (int k = j; k < nElems; k++) {
+          _arr[k] = _arr[k + 1];
+        }
+        nElems--;
+        return true;
+      }
+    }
+    //*********************************************
+    public void display() {
+      for (int j = 0; j < nElems; j++) {
+        Console.Write(_arr[j] + " ");
+        Console.WriteLine("");
+      }
+    }
+
+
+  }
+}
